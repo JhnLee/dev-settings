48ab2d7a971c037e2dd0c83290dcafb867683b26
jhnlee <1620497@gmail.com>
2020-08-20 10:55:57 +0900

    Update GPU install settings

--- gpu_settings/set-gpu-driver.sh
+++ gpu_settings/set-gpu-driver.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade -y
-uname -m && cat /etc/*release
-    sudo apt-get install build-essential 
-sudo apt-get install cmake git unzip zip
-sudo apt-get install python-dev python3-dev python-pip python3-pip
-sudo apt-get install linux-headers-$(uname -r)
+if [ $EUID -ne 0 ]; then
+    echo Run this file as root
+    exit
+fi
 
-sudo apt-get purge nvidia*
-sudo apt-get autoremove
-sudo apt-get autoclean
-sudo rm -rf /usr/local/cuda*
+apt-get update && sudo apt-get upgrade -y
+apt-get install build-essential 
+apt-get install cmake git unzip zip
+apt-get install python-dev python3-dev python-pip python3-pip
+apt-get install linux-headers-$(uname -r)
 
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+apt-get purge -y nvidia*
+apt-get autoremove
+apt-get autoclean
+rm -rf /usr/local/cuda*
+
+apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 
 echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
 
-sudo apt-get update 
-sudo apt-get -o Dpkg::Options::="--force-overwrite" install cuda-10-0 cuda-drivers
+apt-get update 
+apt-get -o Dpkg::Options::="--force-overwrite" install -y cuda-10-1 cuda-drivers
+
+if [ -f ~/.zshrc ]; then
+   shell=~/.zshrc
+elif [ -f ~/.bashrc ]; then
+   shell=~/.bashrc
+else
+    echo Invalid shell environment
+    exit
+fi
+echo 'export PATH=/usr/local/cuda-10.1/bin:$PATH' >> ${shell}
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64:$LD_LIBRARY_PATH' >> ${shell}
+source ${shell} && ldconfig
+
 
 
